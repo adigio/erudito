160628201c712b16e5ec417830f1e314ee17b693
Matias <matiasalarcon55@hotmail.com>
2021-05-18 20:26:01 -0300

    Ahora si va a generar un numero aleatorio de 6 cifras. Observen que los tests fallan

--- src/main/java/ar/edu/unlam/erudito/servicios/ServicioJuegoImpl.java
+++ src/main/java/ar/edu/unlam/erudito/servicios/ServicioJuegoImpl.java
@@ -10,8 +10,8 @@ public class ServicioJuegoImpl implements ServicioJuego {
 
     @Override
     public Integer generarCodigo() {
-        Integer minimo=1;
-        Integer maximo=10;
+        Integer minimo=100000;
+        Integer maximo=999999;
         Integer codigo=(int)(Math.random()*(maximo-minimo)+minimo);
         return codigo;
     }
