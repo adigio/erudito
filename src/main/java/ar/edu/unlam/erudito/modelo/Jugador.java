f86f4ec40685d65b325482143ca1ea2ba46b7214
Matias <matiasalarcon55@hotmail.com>
2021-05-26 16:28:30 -0300

    Se agrego puntos acumulados

--- src/main/java/ar/edu/unlam/erudito/modelo/Jugador.java
+++ src/main/java/ar/edu/unlam/erudito/modelo/Jugador.java
@@ -1,15 +1,18 @@
 package ar.edu.unlam.erudito.modelo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Jugador {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    private Integer puntosAcumulados=0;
 
     public Long getId() {
         return id;
@@ -25,5 +28,13 @@ public class Jugador {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Integer getPuntosAcumulados() {
+        return puntosAcumulados;
+    }
+
+    public void setPuntosAcumulados(Integer puntosAcumulados) {
+        this.puntosAcumulados = puntosAcumulados;
     }
 }
